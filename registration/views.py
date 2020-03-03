57aeb06100e035cf023f982045087bea62112ff6
Sakhile Masoka <smasoka@csir.co.za>
2020-03-03 16:12:43 +0200

    registration index view

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Index page
+def index(request):
+    return HttpResponse("Home Page for CHPC registration")
